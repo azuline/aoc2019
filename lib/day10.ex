048cb67b0a4d0ed4dab8479656e4d08c76336ab3
azuline <azuline@riseup.net>
2019-12-17 20:33:09 -0500

    d10p2

--- lib/day10.ex
+++ lib/day10.ex
@@ -17,12 +17,31 @@ defmodule Day10 do
     map = get_map()
 
     IO.puts("Part 1: #{part1(map)}")
+    IO.puts("Part 2: #{part2(map)}")
   end
 
   def part1(map) do
     asteroids = construct_asteroids_map(map)
     dimensions = get_asteroids_dimensions(map)
 
+    get_monitoring_station_coords(asteroids, dimensions)
+    |> (&elem(&1, 1)).()
+  end
+
+  def part2(map) do
+    asteroids = construct_asteroids_map(map)
+    dimensions = get_asteroids_dimensions(map)
+
+    ms_coords =
+      get_monitoring_station_coords(asteroids, dimensions)
+      |> (&elem(&1, 0)).()
+
+    sort_asteroids_into_list(asteroids, ms_coords)
+    |> get_200th_asteroid_coords()
+    |> (fn {x, y} -> 100 * x + y end).()
+  end
+
+  def get_monitoring_station_coords(asteroids, dimensions) do
     asteroids
     |> Stream.map(fn {coords, is_asteroid} ->
       num_visible =
@@ -33,12 +52,11 @@ defmodule Day10 do
       {coords, num_visible}
     end)
     |> Enum.max_by(&elem(&1, 1))
-    |> (&elem(&1, 1)).()
   end
 
   def construct_asteroids_map(map) do
-    for {row, x} <- Enum.with_index(map),
-        {is_asteroid, y} <- Enum.with_index(row),
+    for {row, y} <- Enum.with_index(map),
+        {is_asteroid, x} <- Enum.with_index(row),
         into: %{},
         do: {{x, y}, is_asteroid}
   end
@@ -56,6 +74,46 @@ defmodule Day10 do
          else: 0
     end
     |> Enum.sum()
+  end
+
+  def sort_asteroids_into_list(asteroids, {ms_x, ms_y} = ms_coords) do
+    asteroids
+    |> Map.to_list()
+    |> Stream.reject(&(elem(&1, 0) == ms_coords))
+    |> Stream.reject(&(elem(&1, 1) == false))
+    |> Enum.map(fn {coords, _} -> {calculate_slope_weight(coords, ms_coords), coords} end)
+    |> transform_slope_weights_into_map()
+    |> Map.to_list()
+    |> Enum.sort(fn {{weight1, abs_slope1}, _}, {{weight2, abs_slope2}, _} ->
+      if weight1 == weight2, do: abs_slope1 < abs_slope2, else: weight1 < weight2
+    end)
+    |> Enum.map(fn {_, coords} ->
+      Enum.sort_by(coords, fn {x, y} -> :math.pow(x - ms_x, 2) + :math.pow(y - ms_y, 2) end)
+    end)
+  end
+
+  def get_200th_asteroid_coords([[asteroid | asteroids_list] | coords_list], ctr \\ 1) do
+    cond do
+      ctr == 200 ->
+        asteroid
+
+      asteroids_list == [] ->
+        get_200th_asteroid_coords(coords_list, ctr + 1)
+
+      true ->
+        get_200th_asteroid_coords(coords_list ++ [asteroids_list], ctr + 1)
+    end
+  end
+
+  def calculate_slope_weight({x, y}, {ms_x, ms_y}) do
+    diff = %{x: x - ms_x, y: y - ms_y}
+
+    cond do
+      diff.x >= 0 and diff.y < 0 -> {0, abs(diff.x / diff.y)}
+      diff.x > 0 and diff.y >= 0 -> {1, diff.y / diff.x}
+      diff.x <= 0 and diff.y > 0 -> {2, abs(diff.x / diff.y)}
+      diff.x < 0 and diff.y <= 0 -> {3, diff.y / diff.x}
+    end
   end
 
   # Recursive function to check for asteroids that block line of sight.
@@ -82,5 +140,15 @@ defmodule Day10 do
       1 -> {mx, my}
       gcd -> {trunc(mx / gcd), trunc(my / gcd)}
     end
+  end
+
+  defp transform_slope_weights_into_map(weights, map \\ %{})
+
+  defp transform_slope_weights_into_map([], map), do: map
+
+  defp transform_slope_weights_into_map([{weight, coords} | weights], map) do
+    map = Map.update(map, weight, [coords], &[coords | &1])
+
+    transform_slope_weights_into_map(weights, map)
   end
 end
--- test/day10_test.exs
+++ test/day10_test.exs
@@ -7,6 +7,10 @@ defmodule Day10Test do
     assert 319 == get_map() |> part1()
   end
 
+  test "part 2" do
+    assert 517 == get_map() |> part2()
+  end
+
   def parse_map(map) do
     map
     |> Stream.map(&String.graphemes/1)
@@ -43,5 +47,81 @@ defmodule Day10Test do
       ])
 
     assert 33 == part1(map)
+  end
+
+  defp rel(x, y), do: {5 + x, 5 + y}
+
+  test "sort asteroid to list" do
+    ms_coord = {2, 2}
+
+    asteroids =
+      parse_map([
+        ".#..#",
+        ".....",
+        "#####",
+        "....#",
+        "...##"
+      ])
+      |> construct_asteroids_map()
+
+    asteroids_list = sort_asteroids_into_list(asteroids, ms_coord)
+
+    assert asteroids_list == [
+             [{4, 0}],
+             [{3, 2}, {4, 2}],
+             [{4, 3}],
+             [{4, 4}],
+             [{3, 4}],
+             [{1, 2}, {0, 2}],
+             [{1, 0}]
+           ]
+  end
+
+  test "get 200th asteroid" do
+    asteroids = [[0, :asteroid]] ++ for i <- 1..198, do: [i]
+
+    assert :asteroid == get_200th_asteroid_coords(asteroids)
+  end
+
+  # Remark: 0,0 coord is in the top left.
+
+  test "calculate slope weight top right sector" do
+    ms_coord = {2, 2}
+
+    weight1 = calculate_slope_weight(rel(3, -1), ms_coord)
+    weight2 = calculate_slope_weight(rel(-1, 3), ms_coord)
+
+    assert weight1 == {1, 1 / 3}
+    assert weight2 == {1, 3}
+  end
+
+  test "calculate slope weight bottom right sector" do
+    ms_coord = {5, 5}
+
+    weight1 = calculate_slope_weight(rel(3, 1), ms_coord)
+    weight2 = calculate_slope_weight(rel(1, 3), ms_coord)
+
+    assert weight1 == {1, 1 / 3}
+    assert weight2 == {1, 3}
+  end
+
+  test "calculate slope weight bottom left sector" do
+    ms_coord = {5, 5}
+
+    weight1 = calculate_slope_weight(rel(-1, 3), ms_coord)
+    weight2 = calculate_slope_weight(rel(-3, 1), ms_coord)
+
+    assert weight1 == {2, 1 / 3}
+    assert weight2 == {2, 3}
+  end
+
+  test "calculate slope weight top left sector" do
+    ms_coord = {5, 5}
+
+    weight1 = calculate_slope_weight(rel(-3, -1), ms_coord)
+    weight2 = calculate_slope_weight(rel(-1, -3), ms_coord)
+
+    assert weight1 == {3, 1 / 3}
+    assert weight2 == {3, 3}
   end
 end
