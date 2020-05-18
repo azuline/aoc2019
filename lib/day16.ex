cd938e33ec12bd0dba8399fa4df77cf87d3ab338
azuline <azuline@riseup.net>
2020-05-18 14:28:02 -0400

    d16p2

--- lib/day16.ex
+++ lib/day16.ex
@@ -4,7 +4,7 @@ defmodule Day16 do
   Day 16: Flawed Frequency Transmission
   """
 
-  alias Day16.Part1
+  alias Day16.{Part1, Part2}
 
   def get_signal() do
     Path.join(__DIR__, "inputs/day16.txt")
@@ -18,6 +18,7 @@ defmodule Day16 do
     signal = get_signal()
 
     IO.puts("Part 1: #{Part1.run(signal)}")
+    IO.puts("Part 2: #{Part2.run(signal)}")
   end
 end
 
@@ -26,6 +27,7 @@ defmodule Day16.Part1 do
     Enum.reduce(1..100, signal, fn _, signal -> run_fft_algo(signal) end)
     |> Enum.take(8)
     |> Enum.join()
+    |> String.to_integer()
   end
 
   def run_fft_algo(signal) do
@@ -46,5 +48,53 @@ defmodule Day16.Part1 do
       2 -> 0
       3 -> -1
     end
+  end
+end
+
+defmodule Day16.Part2 do
+  @moduledoc """
+  It is clearly not feasible to actually brute force the FFT with 10000
+  repetitions of the input, considering that the calculation is O(n^2).
+
+  There are 651 characters in the input. That means our "real" input signal has
+  6_510_000 digits. The first seven digits of our input, aka our offset, are
+  5_977_341.
+
+  What is notable is that the latter half of the signal can be computed quickly
+  in reverse via partial sums, because the coefficients are always `1`.
+  Furthermore, in the latter half, each digit in the output only relies on the
+  digits after its position in the input. Since the offset is in the second
+  half of the signal, we can save on some annoying computations.
+  """
+
+  def run(signal) do
+    full_signal =
+      signal
+      |> List.duplicate(10000)
+      |> List.flatten()
+      |> Enum.drop(signal |> get_offset())
+
+    Enum.reduce(1..100, full_signal, fn _, signal -> rtl_prefix_sums(signal) end)
+    |> Enum.take(8)
+    |> Enum.join()
+    |> String.to_integer()
+  end
+
+  def get_offset(signal) do
+    signal
+    |> Enum.take(7)
+    |> Enum.join()
+    |> String.to_integer()
+  end
+
+  def rtl_prefix_sums(signal) do
+    signal
+    |> Enum.reverse()
+    |> Enum.reduce({[], 0}, fn digit, {prefixes, sum} ->
+      new_sum = (digit + sum) |> Integer.mod(10)
+
+      {[new_sum | prefixes], new_sum}
+    end)
+    |> (&elem(&1, 0)).()
   end
 end
--- test/day16_test.exs
+++ test/day16_test.exs
@@ -2,11 +2,29 @@ defmodule Day16Test do
   use ExUnit.Case, async: true
 
   import Day16
-  alias Day16.Part1
+  alias Day16.{Part1, Part2}
 
   # Takes a few seconds to run.
 
   # test "part 1" do
   #   assert 12541048 == get_signal() |> Part1.run()
   # end
+
+  test "part 1 example 1" do
+    digits =
+      "80871224585914546619083218645595"
+      |> String.graphemes()
+      |> Enum.map(&String.to_integer/1)
+
+    assert 24_176_176 == digits |> Part1.run()
+  end
+
+  test "part 2 example 1" do
+    digits =
+      "03036732577212944063491565474664"
+      |> String.graphemes()
+      |> Enum.map(&String.to_integer/1)
+
+    assert 84_462_026 == digits |> Part2.run()
+  end
 end
